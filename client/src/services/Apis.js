df315a20595f52ccb125a583e13bf51282a87aa0
Shruti Gangwar <75498086+SHRUTI82@users.noreply.github.com>
2023-04-30 03:40:37 +0530

    Delete client directory

--- client/src/services/Apis.js
+++
@@ -1,7 +0,0 @@
-import { commonrequest } from "./ApiCalls";
-import {BACKEND_URL} from "./helper";
-
-
-export const registerfunction = async(data)=>{
-    return await commonrequest("POST",`${BACKEND_URL}/register`,data)
-}
